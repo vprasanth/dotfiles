2e8f36009fd20c29fc804649c5b5ab906e50535e
Prasanth Vaaheeswaran <prasanthv@icloud.com>
2024-03-28 22:45:42 -0400

    chore: changes oh my

--- stow/nvim/.config/nvim/init.lua
+++ stow/nvim/.config/nvim/init.lua
@@ -97,6 +97,18 @@ require("mason-lspconfig").setup({
 	handlers = handlers,
 })
 
+local nvim_lsp = require('lspconfig')
+nvim_lsp.denols.setup {
+  on_attach = on_attach,
+  root_dir = nvim_lsp.util.root_pattern("deno.json", "deno.jsonc"),
+}
+
+nvim_lsp.tsserver.setup {
+  on_attach = on_attach,
+  root_dir = nvim_lsp.util.root_pattern("package.json"),
+  single_file_support = false
+}
+
 require("nvim-tree").setup({
 	disable_netrw = true,
 	hijack_netrw = true,
